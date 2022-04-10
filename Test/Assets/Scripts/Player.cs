cb09d73e6bf9dc6a1002464b0874af016887dbda
Andreas Ranzmaier <a.ranzmaier@barbaric.at>
2022-04-10 09:44:30 +0200

    more tutorial + programming setting

--- Test/Assets/Scripts/Player.cs
+++ Test/Assets/Scripts/Player.cs
@@ -4,20 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Player : MonoBehaviour{
-    public int upForce = 5;
+public class Player : MonoBehaviour
+{
+    private bool jumpKeyPressed;
+    static public int upForcePlayer = 5;
+    Vector3 V3upForce = new Vector3(0, upForcePlayer, 0);
+    private float horizontalInput;
+
     // Start is called before the first frame update
-    void Start(){
-        
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update(){
-        // KeyDown only checking it once
-        if (Input.GetKeyDown(KeyCode.Space)){
-            //Debug.Log("Space");
-            Vector3 V3upForce = new Vector3(0,upForce,0);
+    void Update()
+    {
+        
+        // -- Jump -- 
+        // get button would also work just fine // KeyDown only checking it once
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            jumpKeyPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("horizontal");
+    }
+
+    // FixedUpdate is called once every physics update
+    void FixedUpdate()
+    {
+        if (jumpKeyPressed)
+        {
             GetComponent<Rigidbody>().AddForce(V3upForce, ForceMode.VelocityChange);
+            jumpKeyPressed = false;
         }
     }
 }
