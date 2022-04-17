3a373a620732b07d7d551ec27d359fbf3467fa92
RanzmaierAndreas <31857327+AndreasRanzmaier@users.noreply.github.com>
2022-04-17 17:25:18 +0200

    adding pipe delete when to far left

--- FlappyBird2D/Assets/Scripts/PipeSpawn.cs
+++ FlappyBird2D/Assets/Scripts/PipeSpawn.cs
@@ -6,24 +6,24 @@ public class PipeSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject Pipe;
     private Rigidbody2D RigidbodyComponent;
-
     [SerializeField] private int speed;
-    private GameObject[] arrPipes;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         bool enoughSpace = true;
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
-            if(child.transform.position.x > 4)
+            // check if we shall span another one
+            if (child.transform.position.x > 4)
             {
                 enoughSpace = false;
+            }
+
+            // delete if to far left
+            if (child.transform.position.x < -11)
+            {
+                Destroy(child.gameObject);
             }
         }
 
@@ -39,15 +39,11 @@ public class PipeSpawn : MonoBehaviour
         // +-3.9 = max up/down 
         // 8.68 = max right
         // 10.63 = spawn right out of sight
-        var PipeNew = Instantiate(Pipe, new Vector3(10.63f, 0, 0), Quaternion.identity);
+        float sudoRandom = Random.Range(-1f, 1f);
+        var PipeNew = Instantiate(Pipe, new Vector3(10.63f, sudoRandom, 0), Quaternion.identity);
         PipeNew.transform.parent = gameObject.transform;
 
         RigidbodyComponent = PipeNew.GetComponent<Rigidbody2D>();
         RigidbodyComponent.AddForce(Vector2.left * speed, ForceMode2D.Force);
-    }
-
-    void OnBecameInvisible()
-    {
-        // Destroy(GameObject);
     }
 }
